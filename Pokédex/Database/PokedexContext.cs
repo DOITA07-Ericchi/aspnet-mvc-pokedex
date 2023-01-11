b38d4a241efefee165e316c89daad30bf203bbf4
L-V-Dragon <viotti37@gmail.com>
2023-01-11 10:15:53 +0100

    Context Creato

--- Pokédex/Database/PokedexContext.cs
+++ Pokédex/Database/PokedexContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Pokédex.Database
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pokédex.Database
 {
-	public class PokedexContext
+	public class PokedexContext : DbContext
 	{
+		public DbSet<Pokemon> Pokemons { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=localhost;Database=PokedexV1;" +
+			"Integrated Security=True;TrustServerCertificate=True");
+		}
 	}
 }
