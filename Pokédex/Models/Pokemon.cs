45aebfb2836161c6d95c40ab8cfe2d2b4e58ef18
L-V-Dragon <viotti37@gmail.com>
2023-01-11 10:37:59 +0100

    Model Creato

--- Pokédex/Models/Pokemon.cs
+++ Pokédex/Models/Pokemon.cs
@@ -1,6 +1,47 @@
-﻿namespace Pokédex.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pokédex.Models
 {
 	public class Pokemon
 	{
+		public int ID { get; set; }
+
+		[Column(TypeName = "integer")]
+		public int No { get; set; }
+
+		[Column(TypeName = "varchar(10)")]
+		public string Nome { get; set; }
+
+		[Column(TypeName = "text")]
+		public string Descrizione { get; set; }
+
+		[Column(TypeName = "tinyint")]
+		public int Tipo1 { get; set; }
+
+		[Column(TypeName = "tinyint")]
+		public int Tipo2 { get; set; }
+
+		[Column(TypeName = "varchar(100)")]
+		public string Immagine { get; set; }
+
+		[Column(TypeName = "tinyint")]
+		public int Stato { get; set; }
+
+		public Pokemon()
+		{
+
+		}
+
+		public Pokemon(int id, string nome, string descrizione, int tipo1, int tipo2, string immagine, int stato)
+		{
+			ID = id;
+			Nome = nome;
+			Descrizione = descrizione;
+			Tipo1 = tipo1;
+			Tipo2 = tipo2;
+			Immagine = immagine;
+			Stato = stato;
+		}
 	}
 }
